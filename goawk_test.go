e2f9ac0da4a8ad5e1fb3fd354b499599f73ce20c
Ben Hoyt <benhoyt@gmail.com>
2018-07-10 20:37:57 -0400

    Add one-true-awk tests and expected output

--- goawk_test.go
+++ goawk_test.go
@@ -4,9 +4,11 @@ package main_test
 
 import (
 	"bytes"
+	"flag"
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"testing"
 
@@ -14,38 +16,63 @@ import (
 	"github.com/benhoyt/goawk/parser"
 )
 
-// TODO: don't hard-code these paths
-func TestP(t *testing.T) {
-	infos, err := ioutil.ReadDir("/Users/ben.hoyt/Home/awk/regdir")
+var (
+	testsDir  string
+	outputDir string
+)
+
+func TestMain(m *testing.M) {
+	flag.StringVar(&testsDir, "testsdir", "./tests", "directory with one-true-awk tests")
+	flag.StringVar(&outputDir, "outputdir", "./tests/output", "directory for test output")
+	flag.Parse()
+	os.Exit(m.Run())
+}
+
+func TestAgainstOneTrueAWK(t *testing.T) {
+	inputByPrefix := map[string]string{
+		"t": "test.data",
+		"p": "test.countries",
+	}
+	nonzeroExits := map[string]bool{
+		"t.exit":  true,
+		"t.exit1": true,
+	}
+
+	infos, err := ioutil.ReadDir(testsDir)
 	if err != nil {
 		t.Fatalf("couldn't read test files: %v", err)
 	}
 	for _, info := range infos {
-		if !strings.HasPrefix(info.Name(), "t.") {
+		if !strings.HasPrefix(info.Name(), "t.") && !strings.HasPrefix(info.Name(), "p.") {
 			continue
 		}
-		srcPath := "/Users/ben.hoyt/Home/awk/regdir/" + info.Name()
-		inputPath := "/Users/ben.hoyt/Home/awk/regdir/test.data"
 		t.Run(info.Name(), func(t *testing.T) {
-			// TODO: should these t.Error be t.Fatal?
+			srcPath := filepath.Join(testsDir, info.Name())
+			inputPath := filepath.Join(testsDir, inputByPrefix[info.Name()[:1]])
+			outputPath := filepath.Join(outputDir, info.Name())
 			cmd := exec.Command("awk", "-f", srcPath, inputPath)
 			expected, err := cmd.Output()
-			if err != nil {
-				t.Error(err)
+			if err != nil && !nonzeroExits[info.Name()] {
+				t.Fatalf("error running awk: %v", err)
 			}
-
-			output, err := execute(srcPath, inputPath)
+			err = ioutil.WriteFile(outputPath, expected, 0644)
 			if err != nil {
-				t.Error(err)
-			} else if string(output) != string(expected) {
-				t.Errorf("output incorrect")
-				//t.Errorf("got first block instead of second (expected):\n%s---\n%s", output, expected)
+				t.Fatalf("error writing awk output: %v", err)
 			}
+			// output, err := executeGoAWK(srcPath, inputPath)
+			// if err != nil {
+			// 	t.Fatal(err)
+			// } else if string(output) != string(expected) {
+			// 	t.Fatalf("got first block instead of second (expected):\n%s---\n%s", output, expected)
+			// }
 		})
 	}
+
+	_ = os.Remove("tempbig")
+	_ = os.Remove("tempsmall")
 }
 
-func execute(srcPath, inputPath string) ([]byte, error) {
+func executeGoAWK(srcPath, inputPath string) ([]byte, error) {
 	src, err := ioutil.ReadFile(srcPath)
 	if err != nil {
 		return nil, err
