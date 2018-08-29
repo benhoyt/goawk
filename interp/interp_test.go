19c7a9c5bdf36d5b4042edbcdba510efabd1bbf2
Ben Hoyt <benhoyt@gmail.com>
2018-08-29 07:10:46 -0400

    Tweak for loop test to be more like the others

--- interp/interp_test.go
+++ interp/interp_test.go
@@ -787,7 +787,9 @@ BEGIN {
 func BenchmarkForLoop(b *testing.B) {
 	benchmarkProgram(b, "", "", `
 BEGIN {
-  for (i = 0; i < %d; i++);
+  for (i = 0; i < %d; i++) {
+  	for (j = 0; j < 100; j++);
+  }
 }
 `, b.N)
 }
