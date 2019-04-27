8b033f6ba93c36b9630725a419395011aba0b81c
Ben Hoyt <benhoyt@gmail.com>
2019-04-27 16:48:40 -0500

    Add script to build cross-platform binaries
    Windows, Linux, macOS in 32- and 64-bit variants
    
    Fixes #23 (thanks @cup)

---
+++ make_binaries.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+go build
+VERSION="$(./goawk -version)"
+
+GOOS=windows GOARCH=386 go build
+zip "goawk_${VERSION}_windows_386.zip" goawk.exe README.md LICENSE.txt
+GOOS=windows GOARCH=amd64 go build
+zip "goawk_${VERSION}_windows_amd64.zip" goawk.exe README.md LICENSE.txt
+
+GOOS=linux GOARCH=386 go build
+tar -cvzf "goawk_${VERSION}_linux_386.tar.gz" goawk README.md LICENSE.txt
+GOOS=linux GOARCH=amd64 go build
+tar -cvzf "goawk_${VERSION}_linux_amd64.tar.gz" goawk README.md LICENSE.txt
+
+GOOS=darwin GOARCH=386 go build
+tar -cvzf "goawk_${VERSION}_darwin_386.tar.gz" goawk README.md LICENSE.txt
+GOOS=darwin GOARCH=amd64 go build
+tar -cvzf "goawk_${VERSION}_darwin_amd64.tar.gz" goawk README.md LICENSE.txt
+
+rm -f goawk goawk.exe
