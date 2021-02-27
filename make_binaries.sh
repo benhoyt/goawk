f1bdc5e09b8a84d4af13ee67cf542412d534389e
Ben Hoyt <benhoyt@gmail.com>
2021-02-27 17:04:18 +1300

    remove darwin/386 (no longer supported)

--- make_binaries.sh
+++ make_binaries.sh
@@ -13,8 +13,6 @@ tar -cvzf "goawk_${VERSION}_linux_386.tar.gz" goawk README.md LICENSE.txt
 GOOS=linux GOARCH=amd64 go build
 tar -cvzf "goawk_${VERSION}_linux_amd64.tar.gz" goawk README.md LICENSE.txt
 
-GOOS=darwin GOARCH=386 go build
-tar -cvzf "goawk_${VERSION}_darwin_386.tar.gz" goawk README.md LICENSE.txt
 GOOS=darwin GOARCH=amd64 go build
 tar -cvzf "goawk_${VERSION}_darwin_amd64.tar.gz" goawk README.md LICENSE.txt
 
