ca5e3ac4e30501f844979e17c9bc5f09b95df17e
Andrea Fiori <47670182+fioriandrea@users.noreply.github.com>
2021-09-26 06:48:20 +0200

    Make exponents in number literals behave like awk/gawk (#69)
    Co-authored-by: Ben Hoyt <benhoyt@gmail.com>

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -152,9 +152,13 @@ func (l *Lexer) scan() (Position, Token, string) {
 				l.next()
 				gotDigit = true
 			}
-			// Per awk/gawk, "1e" is allowed, but not "1e+"
-			if gotSign && !gotDigit {
-				return l.pos, ILLEGAL, "expected digits"
+			// Per awk/gawk, "1e" is allowed and parsed as "1 e" (with "e"
+			// considered a variable). "1e+" is parsed as "1e + ...".
+			if !gotDigit {
+				if gotSign {
+					l.unread() // unread the '+' or '-'
+				}
+				l.unread() // unread the 'e' or 'E'
 			}
 		}
 		tok = NUMBER
@@ -396,6 +400,7 @@ func (l *Lexer) next() {
 		if l.ch != 0 {
 			l.ch = 0
 			l.offset++
+			l.nextPos.Column++
 		}
 		return
 	}
@@ -408,6 +413,14 @@ func (l *Lexer) next() {
 	}
 	l.ch = ch
 	l.offset++
+}
+
+// Un-read the character just scanned (doesn't handle line boundaries).
+func (l *Lexer) unread() {
+	l.offset--
+	l.pos.Column--
+	l.nextPos.Column--
+	l.ch = l.src[l.offset-1]
 }
 
 func isNameStart(ch byte) bool {
