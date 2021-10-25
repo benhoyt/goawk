7b60dc501346c39de845c7d2eee62d6b5c52452a
Ben Hoyt <benhoyt@gmail.com>
2021-10-25 19:48:41 +1300

    Improve test error handling and remove goawk_test.go line numbers

--- awkgo/run_tests.sh
+++ awkgo/run_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-go test ./awkgo -v | awk '{ sub(/TestAWKGo[0-9]+/, "TestAWKGo"); sub(/( \(|\t)[0-9]+\.[0-9]+s\)?/, ""); print }' >awkgo/tests.txt
+go test ./awkgo -v | awk '{ sub(/TestAWKGo[0-9]+/, "TestAWKGo"); sub(/( \(|\t)[0-9]+\.[0-9]+s\)?/, ""); sub(/awkgo_test.go:[0-9]+/, "awkgo_test.go"); print }' >awkgo/tests.txt
