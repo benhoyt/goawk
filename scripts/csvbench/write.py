07eb4505a7f64cffceeb7d8579acc70ce3e96689
Ben Hoyt <benhoyt@gmail.com>
2022-05-13 23:39:41 +1200

    Update benchmarks to avoid huge.csv (read benchmark uses write output) (#112)

--- scripts/csvbench/write.py
+++ scripts/csvbench/write.py
@@ -2,5 +2,26 @@ import csv
 import sys
 
 writer = csv.writer(sys.stdout)
-for i in range(10000000):
-	writer.writerow([i, "foo", "bob@example.com", "quoted,string", "final field"])
+for i in range(3514073):  # will create a ~1GB file
+	writer.writerow([
+		i,
+		"foo",
+		"bob@example.com",
+		"simple,quoted",
+		"quoted string with \" in it",
+		"0123456789",
+		"9876543210",
+		"The quick brown fox jumps over the lazy dog",
+		"",
+		"final field",
+		i,
+		"foo",
+		"bob@example.com",
+		"simple,quoted",
+		"quoted string with \" in it",
+		"0123456789",
+		"9876543210",
+		"The quick brown fox jumps over the lazy dog",
+		"",
+		"final field",
+	])
