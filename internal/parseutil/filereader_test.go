500a8b102c6527e2f70f14e3ac2f8e16416d94f2
xonix <xonixx@gmail.com>
2022-09-30 18:17:04 +0300

    better filenames tracking : CR

--- internal/parseutil/filereader_test.go
+++ internal/parseutil/filereader_test.go
@@ -9,30 +9,27 @@ import (
 func TestLineResolution(t *testing.T) {
 	fr := &FileReader{}
 
-	file1 := "file1"
-	file2 := "file2"
-
 	addFile := func(fileName string, code string) {
 		if nil != fr.AddFile(fileName, strings.NewReader(code)) {
 			panic("should not happen")
 		}
 	}
 
-	addFile(file1, `BEGIN {
+	addFile("file1", `BEGIN {
 print f(1)
 }`)
-	addFile(file2, `function f(x) {
+	addFile("file2", `function f(x) {
 print x
 }`)
 	{
 		path, line := fr.FileLine(2)
-		if path != file1 || line != 2 {
+		if path != "file1" || line != 2 {
 			t.Errorf("wrong path/line")
 		}
 	}
 	{
 		path, line := fr.FileLine(5)
-		if path != file2 || line != 2 {
+		if path != "file2" || line != 2 {
 			t.Errorf("wrong path/line")
 		}
 	}
