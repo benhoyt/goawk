2640accf285013c0423767ee5c7c32337b5b026d
Ben Hoyt <benhoyt@gmail.com>
2023-04-08 06:02:02 +1200

    When running "gawk --posix", ensure we're using UTF-8 (#174)
    * When running "gawk --posix", ensure we're using UTF-8
    
    This avoids the following test failure when LC_ALL=C:
    
    $ go test ./interp
    --- FAIL: TestInterp (1.80s)
        --- FAIL: TestInterp/awkposix_BEGIN_{_RS="ö"_}__{_print_}__#_!windows-gawk (0.00s)
            interp_test.go:983: expected/got:
                "1\ntwo\nthree\n"
                "1\n\xb6two\n\xb6three\n"
    FAIL
    FAIL	github.com/benhoyt/goawk/interp	1.874s
    FAIL
    
    Also make the same change in goawk_test.go to avoid issues in future
    (though that doesn't fail right now).
    
    Thanks to Jan Mercl for the bug report.
    
    
    * For some reason Windows doesn't like LC_ALL
    
    We get failures such as:
    
    --- FAIL: TestCommandLine/BEGIN_{_print_"1";_"echo_2"_|_getline_x;_print_x_} (0.17s)
        --- FAIL: TestCommandLine/BEGIN_{_print_"1";_"echo_2"_|_getline_x;_print_x_}/awk (0.08s)
            goawk_test.go:456: expected no error, got AWK error: exit status 2 (gawk: cmd. line:1: fatal: cannot create child process for `echo 2' (fork: Bad file descriptor)
                )
    
    There are probably better ways to address this, but this'll do for now.

--- goawk_test.go
+++ goawk_test.go
@@ -489,6 +489,9 @@ func runAWKs(t *testing.T, testArgs []string, testStdin, testOutput, testError s
 		}
 		args = append(args, testArgs...)
 		cmd := exec.Command(awkExe, testArgs...)
+		if runtime.GOOS != "windows" {
+			cmd.Env = []string{"LC_ALL=en_US.UTF-8"}
+		}
 		if testStdin != "" {
 			cmd.Stdin = strings.NewReader(testStdin)
 		}
