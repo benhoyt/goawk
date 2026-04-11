75f823242a090b5e230cbab9012a693f586b627b
Ben Hoyt <benhoyt@gmail.com>
2026-04-11 12:23:33 +1200

    skip /./ tests on gawk for problematic gawk versions
    See https://lists.gnu.org/archive/html/bug-gawk/2026-04/msg00032.html

--- interp/interp_test.go
+++ interp/interp_test.go
@@ -606,7 +606,9 @@ function f() {
 	{`BEGIN { n = split("ab c d ", a); for (i=1; i<=n; i++) print a[i] }`, "", "ab\nc\nd\n", "", ""},
 	{`BEGIN { n = split("ab,c,d,", a, ","); for (i=1; i<=n; i++) print a[i] }`, "", "ab\nc\nd\n\n", "", ""},
 	{`BEGIN { n = split("ab,c.d,", a, /[,.]/); for (i=1; i<=n; i++) print a[i] }`, "", "ab\nc\nd\n\n", "", ""},
-	{`BEGIN { n = split("aaa", a, /./); print n; for (i=1; i<=n; i++) print a[i] }`, "", "4\n\n\n\n\n", "", ""},
+	// Skip these next two on gawk as older gawk had a bug with /./
+	{`BEGIN { n = split("aaa", a, /./); print n; for (i=1; i<=n; i++) print a[i] }  # !gawk`, "", "4\n\n\n\n\n", "", ""},
+	{`{ n = split($0, a, /./); for (i=1; i<=n; i++) { print i, a[i] } }  # !gawk`, "a.a.a\n", "1 \n2 \n3 \n4 \n5 \n6 \n", "", ""},
 	{`BEGIN { n = split("a b\tc", a, / /); print n; for (i=1; i<=n; i++) print a[i] }`, "", "2\na\nb\tc\n", "", ""},
 	{`BEGIN { n = split("1 2", a); print (n, a[1], a[2], a[1]==1, a[2]==2) }`, "", "2 1 2 1 1\n", "", ""},
 	{`BEGIN { x = "1.2.3"; print sub(/\./, ",", x); print x }`, "", "1\n1,2.3\n", "", ""},
