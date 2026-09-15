f59bf0db58a7dd3bebcdd64c9bd75ae2ea10932e
Ben Hoyt <benhoyt@gmail.com>
2026-09-15 15:11:25 +1200

    Bump version to v1.32.0

--- goawk.go
+++ goawk.go
@@ -49,7 +49,7 @@ import (
 )
 
 const (
-	version    = "v1.31.0"
+	version    = "v1.32.0"
 	copyright  = "GoAWK " + version + " - Copyright (c) 2022 Ben Hoyt"
 	shortUsage = "usage: goawk [-F fs] [-v var=value] [-f progfile | 'prog'] [file ...]"
 	longUsage  = `Standard AWK arguments:
